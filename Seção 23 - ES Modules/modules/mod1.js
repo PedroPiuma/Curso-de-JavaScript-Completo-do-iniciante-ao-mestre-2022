1b6f425fcc21e39154b7dcdcee5e968d80954137
Luis Pedro <luis.pedro@essentia.local>
2022-08-23 14:59:25 -0300

    section-23 final

---
+++ Seção 23 - ES Modules/modules/mod1.js
@@ -0,0 +1,11 @@
+const myMod1 = () => {
+    console.log('myMod1 executado')
+}
+
+export function myMod1_nomeada() {
+    return 'funcao exportada nomeada'
+}
+
+export const PI = Math.PI
+
+export default myMod1
